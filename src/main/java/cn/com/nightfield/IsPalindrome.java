0f5a5942d26b42e5d4d53dedeb1d31da0cd4f33c
chingjustwe <ching91@sina.com>
2020-11-08 20:47:27 +0800

    is palindrome

---
+++ src/main/java/cn/com/nightfield/IsPalindrome.java
@@ -0,0 +1,40 @@
+package cn.com.nightfield;
+
+import java.util.Random;
+
+/**
+ * @author: zhochi
+ * @create: 2020/11/8
+ *
+ * https://leetcode-cn.com/problems/palindrome-number/
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ * 思路：
+ * 将数字反转之后，判断是否与原数字相等
+ **/
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x == 0) {
+            return true;
+        }
+
+        int originalX = x, reverseX = x % 10;
+        while ((x /= 10) > 0) {
+            reverseX = reverseX * 10 + x % 10;
+        }
+
+        return originalX == reverseX;
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome isPalindrome = new IsPalindrome();
+
+        System.out.println("121:" + isPalindrome.isPalindrome(121));
+        System.out.println("-121" + isPalindrome.isPalindrome(-121));
+        System.out.println("10" + isPalindrome.isPalindrome(10));
+    }
+}
